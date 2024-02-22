4e24b0c4221aa0460430ec2b65517056441e78cd
PalenovaLiza <palenova-lizochka@mail.ru>
2024-02-22 21:17:39 +0300

    .

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,39 @@
 import './style.scss'
 
-console.log('121')
-console.log('1234')
+//console.log('121')
+//console.log('1233')
+
+//console.log(typeof 12)
+//console.log(typeof '12')
+//console.log(typeof true)
+//console.log(typeof false)
+//console.log(typeof [])
+//console.log(1>2)
+//console.log(1<2)
+
+//let year = 2015
+//if (year==2015){
+ // console.log('yes') 
+//} else{
+ // console.log('no')
+//}
+
+//let value = prompt('Введите число', 0);
+
+//if (value > 0) {
+  //alert( 1 );
+//} else if (value < 0) {
+ // alert( -1 );
+//} else {
+ // alert( 0 );
+//}
+
+//result = (a + b < 4) ? 'Мало' : 'Много';
+
+ 
+  //let message = (login == 'Сотрудник') ? 'Привет' :
+ // (login == 'Директор') ? 'Здравствуйте' :
+  //(login == '') ? 'Нет логина' :
+  //'';
+
+  
